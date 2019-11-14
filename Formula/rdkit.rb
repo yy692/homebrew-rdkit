3a25be622d50bb116468d4aa918a7ddcc02c578f
Yang Yang <31322025+yy692@users.noreply.github.com>
2019-11-14 13:48:30 -0500

    Update rdkit.rb

--- Formula/rdkit.rb
+++ Formula/rdkit.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Rdkit < Formula
   homepage "http://rdkit.org/"
-  url "https://github.com/yy692/rdkit/archive/v0.5-alpha.7.tar.gz"
-  sha256 "9a4a90d285e026e76212c478faea28bae2e4316d5cf91d66b530ac3a72ce250c"
+  url "https://github.com/yy692/rdkit/archive/v0.5-alpha.8.tar.gz"
+  sha256 "2faa136034842fa449475d150d9944cf2c746e864bd2fa90e3fa0d98c8ac8eae"
 
   head do
     url 'https://github.com/yy692/rdkit.git'
